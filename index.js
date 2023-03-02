239317411898f9c4cb10bac4e1850e649c086d29
Synthetica <83266332+The-Synthetica@users.noreply.github.com>
2023-03-01 22:38:52 -0300

    beta

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ onresize = (event) => {
             rootStyles.setProperty('--frame-height', frameHeight+"px");
 };
 onload = (event) => {
+    console.log('loaded');
     
     setTimeout(() => { 
         loader.classList.toggle('loaded');
@@ -95,54 +96,55 @@ function initialize(){
     // Properties
     
     
-    window.CSS.registerProperty({
-        name:'--angulo',
-        syntax: "<angle>",
-        inherits: false,
-        initialValue: '0deg',
-    });
+    
+    // window.CSS.registerProperty({
+    //     name:'--angulo',
+    //     syntax: "<angle>",
+    //     inherits: false,
+    //     initialValue: '0deg',
+    // });
 
-    window.CSS.registerProperty({
-        name:'--ColorAnimation1',
-        syntax: "<color>",
-        inherits: false,
-        initialValue: 'rgb(255,0,255)',
-    });
+    // window.CSS.registerProperty({
+    //     name:'--ColorAnimation1',
+    //     syntax: "<color>",
+    //     inherits: false,
+    //     initialValue: 'rgb(255,0,255)',
+    // });
 
-    window.CSS.registerProperty({
-        name:'--ColorAnimation2',
-        syntax: "<color>",
-        inherits: false,
-        initialValue: 'rgb(0,255,255)',
-    });
+    // window.CSS.registerProperty({
+    //     name:'--ColorAnimation2',
+    //     syntax: "<color>",
+    //     inherits: false,
+    //     initialValue: 'rgb(0,255,255)',
+    // });
 
-    window.CSS.registerProperty({
-        name:'--BGAnimation1',
-        syntax: "<color>",
-        inherits: false,
-        initialValue: 'rgb(51, 55, 85)',
-    });
+    // window.CSS.registerProperty({
+    //     name:'--BGAnimation1',
+    //     syntax: "<color>",
+    //     inherits: false,
+    //     initialValue: 'rgb(51, 55, 85)',
+    // });
 
-    window.CSS.registerProperty({
-        name:'--BGAnimation2',
-        syntax: "<color>",
-        inherits: false,
-        initialValue: 'rgb(51, 55, 85)',
-    });
+    // window.CSS.registerProperty({
+    //     name:'--BGAnimation2',
+    //     syntax: "<color>",
+    //     inherits: false,
+    //     initialValue: 'rgb(51, 55, 85)',
+    // });
 
-    window.CSS.registerProperty({
-        name:'--length1',
-        syntax: "<length>",
-        inherits: false,
-        initialValue: '0px',
-    });
+    // window.CSS.registerProperty({
+    //     name:'--length1',
+    //     syntax: "<length>",
+    //     inherits: false,
+    //     initialValue: '0px',
+    // });
 
-    window.CSS.registerProperty({
-        name:'--length2',
-        syntax: "<length>",
-        inherits: false,
-        initialValue: '7px',
-    });
+    // window.CSS.registerProperty({
+    //     name:'--length2',
+    //     syntax: "<length>",
+    //     inherits: false,
+    //     initialValue: '7px',
+    // });
     
 }
 
@@ -202,15 +204,117 @@ function barChange(cont, flag){
 
 }
 
-// Burger menu uncheck
-for (var i = 0; i < burgerMenuLinks.length; i++) {
-    burgerMenuLinks[i].addEventListener('click', uncheckBurger, false);
+//Animation Calculator
+const elementCalculator= document.getElementById('elementCalculator');
+setInterval(() => { createCalculator() }, 250);
+
+function createCalculator(){
+    const newDiv1= document.createElement('div');
+    const newDiv2= document.createElement('div');
+    const newDiv3= document.createElement('div');
+    const newDiv4= document.createElement('div');
+
+    newDiv1.classList.add('Plus');
+    newDiv2.classList.add('Minus');
+    newDiv3.classList.add('Multiply');
+    newDiv4.classList.add('Divide');
+
+    const offsetLeft1= randomInterval(0, 100);
+    const offsetLeft2= randomInterval(0, 100);
+    const offsetLeft3= randomInterval(0, 100);
+    const offsetLeft4= randomInterval(0, 100);
+
+    const animationTime= randomInterval(2, 5);
+    const animationTime2= randomInterval(2, 5);
+    const animationTime3= randomInterval(2, 5);
+    const animationTime4= randomInterval(2, 5);
+
+    const rotation= randomInterval(-45, 45);
+    const rotation2= randomInterval(-45, 45);
+    const rotation3= randomInterval(-45, 45);
+    const rotation4= randomInterval(-45, 45);
+
+    newDiv1.style.left= offsetLeft1 + '%';
+    newDiv2.style.left= offsetLeft2 + '%';
+    newDiv3.style.left= offsetLeft3 + '%';
+    newDiv4.style.left= offsetLeft4 + '%';
+
+    newDiv1.style.rotate= rotation + 'deg';
+    newDiv2.style.rotate= rotation2 + 'deg';
+    newDiv3.style.rotate= rotation3 + 'deg';
+    newDiv4.style.rotate= rotation4 + 'deg';
+
+    newDiv1.style.animation= 'translate ' + animationTime + 's' + ' linear';
+    newDiv2.style.animation= 'translate ' + animationTime2 + 's' + ' linear';
+    newDiv3.style.animation= 'translate ' + animationTime3 + 's' + ' linear';
+    newDiv4.style.animation= 'translate ' + animationTime4 + 's' + ' linear';
+    
+    elementCalculator.appendChild(newDiv1);
+    elementCalculator.appendChild(newDiv2);
+    elementCalculator.appendChild(newDiv3);
+    elementCalculator.appendChild(newDiv4);
+
+    setTimeout(() => {
+        elementCalculator.removeChild(newDiv1)
+    }, animationTime * 1000);
+
+    setTimeout(() => {
+        elementCalculator.removeChild(newDiv2)
+    }, animationTime2 * 1000);
+
+    setTimeout(() => {
+        elementCalculator.removeChild(newDiv3)
+    }, animationTime3 * 1000);
+
+    setTimeout(() => {
+        elementCalculator.removeChild(newDiv4)
+    }, animationTime4 * 1000);
 }
 
-//Scroll fade elements
-for(var i=0; i < scrollNotVisible.length; i++) {
-    observer.observe(scrollNotVisible[i]);
+// Animation Connect4
+const elementConnect4= document.getElementById('elementConnect4');
+setInterval(() => { createConnect4() }, 250);
+
+function createConnect4(){
+    const newDiv1= document.createElement('div');
+    const newDiv2= document.createElement('div');
+
+    newDiv1.classList.add('token1');
+    newDiv2.classList.add('token2');
+
+    const offsetLeft1= randomInterval(-50, 50);
+    const offsetLeft2= randomInterval(-50, 50);
+    
+    const offsetBottom1= randomInterval(25, 100);
+    const offsetBottom2= randomInterval(25, 100);
+
+    const animationTime= randomInterval(2, 5);
+    const animationTime2= randomInterval(2, 5);
+
+    newDiv1.style.left= offsetLeft1 + '%';
+    newDiv2.style.left= offsetLeft2 + '%';
+
+    newDiv1.style.bottom= ' -' + offsetBottom1 + '%';
+    newDiv2.style.bottom= ' -' + offsetBottom2 + '%';
+
+    newDiv1.style.animation= 'move ' + animationTime + 's' + ' linear';
+    newDiv2.style.animation= 'move ' + animationTime2 + 's' + ' linear';
+    
+    elementConnect4.appendChild(newDiv1);
+    elementConnect4.appendChild(newDiv2);
+
+    setTimeout(() => {
+        elementConnect4.removeChild(newDiv1)
+    }, animationTime * 1000);
+
+    setTimeout(() => {
+        elementConnect4.removeChild(newDiv2)
+    }, animationTime2 * 1000);
 }
+function randomInterval(min, max) {
+    return (Math.random() * (max - min + 1) + min)
+}
+
 
 //Card Follows cursor
 window.addEventListener("mousemove", (e) => {
@@ -277,21 +381,17 @@ window.addEventListener("deviceorientation", (e) => {
     console.log(initFlag,alpha, beta, gamma)
 }, true);
 
-
-
-barLinks[0].addEventListener('click', e => {
+for(let i=0; i<barLinks.length; i++){
+    barLinks[i].addEventListener('click', () =>{
     fullscreenScrolls[cont].style.top= "100vh";
     fullscreenScrolls[cont].style.opacity= "0";
 
-    cont=1;
+    cont=i+1;
     barChange(cont, "");
     
     fullscreenScrolls[cont].style.top= "0";
     fullscreenScrolls[cont].style.opacity= "1";
 
-    // console.log(bar.scrollWidth + 'px' barLinks.length * 2 + 2 + 'rem')
-    // bar.scrollTo(bar.scrollLeft + barLinks[cont-2].offsetWidth, 0);
-    // bar.scrollTo((cont-1)*200, 0);
     bar.scrollTo(0,0);
 
     let dist=0;
@@ -311,106 +411,141 @@ barLinks[0].addEventListener('click', e => {
     flag="stop";
     
     window.scrollTo(0, main.offsetHeight / 4);
-}, false);
+    })
+}
 
-barLinks[1].addEventListener('click', e => {
-    fullscreenScrolls[cont].style.top= "100vh";
-    fullscreenScrolls[cont].style.opacity= "0";
+// barLinks[0].addEventListener('click', e => {
+//     fullscreenScrolls[cont].style.top= "100vh";
+//     fullscreenScrolls[cont].style.opacity= "0";
 
-    cont=2;
-    barChange(cont, "");
+//     cont=1;
+//     barChange(cont, "");
     
-    fullscreenScrolls[cont].style.top= "0";
-    fullscreenScrolls[cont].style.opacity= "1";
+//     fullscreenScrolls[cont].style.top= "0";
+//     fullscreenScrolls[cont].style.opacity= "1";
 
-    // console.log(bar.scrollWidth + 'px' barLinks.length * 2 + 2 + 'rem')
-    // bar.scrollTo(bar.scrollLeft + barLinks[cont-2].offsetWidth, 0);
-    bar.scrollTo(0, 0);
+//     // console.log(bar.scrollWidth + 'px' barLinks.length * 2 + 2 + 'rem')
+//     // bar.scrollTo(bar.scrollLeft + barLinks[cont-2].offsetWidth, 0);
+//     // bar.scrollTo((cont-1)*200, 0);
+//     bar.scrollTo(0,0);
 
-    let dist=0;
-    for(let i=0; i<cont-1; i++){
-        dist+=barLinks[i].offsetWidth + 30;
-    }
+//     let dist=0;
+//     for(let i=0; i<cont-1; i++){
+//         dist+=barLinks[i].offsetWidth + 30;
+//     }
 
-    bar.scrollTo(dist, 0);
+//     bar.scrollTo(dist, 0);
 
-    page.style.overflow="hidden";
+//     page.style.overflow="hidden";
     
-    setTimeout(() => { 
-        flag = " ";
-        page.style.overflow="auto";
-    }, 1000);
+//     setTimeout(() => { 
+//         flag = " ";
+//         page.style.overflow="auto";
+//     }, 1000);
 
-    flag="stop";
+//     flag="stop";
     
-    window.scrollTo(0, main.offsetHeight / 4);
-}, false);
+//     window.scrollTo(0, main.offsetHeight / 4);
+// }, false);
 
-barLinks[2].addEventListener('click', e => {
-    fullscreenScrolls[cont].style.top= "100vh";
-    fullscreenScrolls[cont].style.opacity= "0";
+// barLinks[1].addEventListener('click', e => {
+//     fullscreenScrolls[cont].style.top= "100vh";
+//     fullscreenScrolls[cont].style.opacity= "0";
 
-    cont=3;
-    barChange(cont, "");
+//     cont=2;
+//     barChange(cont, "");
     
-    fullscreenScrolls[cont].style.top= "0";
-    fullscreenScrolls[cont].style.opacity= "1";
+//     fullscreenScrolls[cont].style.top= "0";
+//     fullscreenScrolls[cont].style.opacity= "1";
 
-    // console.log(bar.scrollWidth + 'px' barLinks.length * 2 + 2 + 'rem')
-    // bar.scrollTo(bar.scrollLeft + barLinks[cont-2].offsetWidth, 0);
-    bar.scrollTo(0, 0);
+//     // console.log(bar.scrollWidth + 'px' barLinks.length * 2 + 2 + 'rem')
+//     // bar.scrollTo(bar.scrollLeft + barLinks[cont-2].offsetWidth, 0);
+//     bar.scrollTo(0, 0);
 
-    let dist=0;
-    for(let i=0; i<cont-1; i++){
-        dist+=barLinks[i].offsetWidth + 30;
-    }
+//     let dist=0;
+//     for(let i=0; i<cont-1; i++){
+//         dist+=barLinks[i].offsetWidth + 30;
+//     }
 
-    bar.scrollTo(dist, 0);
+//     bar.scrollTo(dist, 0);
 
-    page.style.overflow="hidden";
+//     page.style.overflow="hidden";
     
-    setTimeout(() => { 
-        flag = " ";
-        page.style.overflow="auto";
-    }, 1000);
+//     setTimeout(() => { 
+//         flag = " ";
+//         page.style.overflow="auto";
+//     }, 1000);
 
-    flag="stop";
+//     flag="stop";
     
-    window.scrollTo(0, main.offsetHeight / 4);
-}, false);
+//     window.scrollTo(0, main.offsetHeight / 4);
+// }, false);
 
-barLinks[3].addEventListener('click', e => {
-    fullscreenScrolls[cont].style.top= "100vh";
-    fullscreenScrolls[cont].style.opacity= "0";
+// barLinks[2].addEventListener('click', e => {
+//     fullscreenScrolls[cont].style.top= "100vh";
+//     fullscreenScrolls[cont].style.opacity= "0";
 
-    cont=4;
-    barChange(cont, "");
+//     cont=3;
+//     barChange(cont, "");
     
-    fullscreenScrolls[cont].style.top= "0";
-    fullscreenScrolls[cont].style.opacity= "1";
+//     fullscreenScrolls[cont].style.top= "0";
+//     fullscreenScrolls[cont].style.opacity= "1";
 
-    // console.log(bar.scrollWidth + 'px' barLinks.length * 2 + 2 + 'rem')
-    // bar.scrollTo(bar.scrollLeft + barLinks[cont-2].offsetWidth, 0);
-    bar.scrollTo(0, 0);
+//     // console.log(bar.scrollWidth + 'px' barLinks.length * 2 + 2 + 'rem')
+//     // bar.scrollTo(bar.scrollLeft + barLinks[cont-2].offsetWidth, 0);
+//     bar.scrollTo(0, 0);
 
-    let dist=0;
-    for(let i=0; i<cont-1; i++){
-        dist+=barLinks[i].offsetWidth + 30;
-    }
+//     let dist=0;
+//     for(let i=0; i<cont-1; i++){
+//         dist+=barLinks[i].offsetWidth + 30;
+//     }
 
-    bar.scrollTo(dist, 0);
+//     bar.scrollTo(dist, 0);
 
-    page.style.overflow="hidden";
+//     page.style.overflow="hidden";
     
-    setTimeout(() => { 
-        flag = " ";
-        page.style.overflow="auto";
-    }, 1000);
+//     setTimeout(() => { 
+//         flag = " ";
+//         page.style.overflow="auto";
+//     }, 1000);
 
-    flag="stop";    
+//     flag="stop";
     
-    window.scrollTo(0, main.offsetHeight / 4);
-}, false);
+//     window.scrollTo(0, main.offsetHeight / 4);
+// }, false);
+
+// barLinks[3].addEventListener('click', e => {
+//     fullscreenScrolls[cont].style.top= "100vh";
+//     fullscreenScrolls[cont].style.opacity= "0";
+
+//     cont=4;
+//     barChange(cont, "");
+    
+//     fullscreenScrolls[cont].style.top= "0";
+//     fullscreenScrolls[cont].style.opacity= "1";
+
+//     // console.log(bar.scrollWidth + 'px' barLinks.length * 2 + 2 + 'rem')
+//     // bar.scrollTo(bar.scrollLeft + barLinks[cont-2].offsetWidth, 0);
+//     bar.scrollTo(0, 0);
+
+//     let dist=0;
+//     for(let i=0; i<cont-1; i++){
+//         dist+=barLinks[i].offsetWidth + 30;
+//     }
+
+//     bar.scrollTo(dist, 0);
+
+//     page.style.overflow="hidden";
+    
+//     setTimeout(() => { 
+//         flag = " ";
+//         page.style.overflow="auto";
+//     }, 1000);
+
+//     flag="stop";    
+    
+//     window.scrollTo(0, main.offsetHeight / 4);
+// }, false);
 
 linkWelcomeSection.addEventListener('click', e => {
     fullscreenScrolls[cont].style.top= "100vh";
@@ -502,4 +637,12 @@ window.addEventListener('scroll', e => {
 });
 
 
-//scalamiento numerico 
+// Burger menu uncheck
+for (var i = 0; i < burgerMenuLinks.length; i++) {
+    burgerMenuLinks[i].addEventListener('click', uncheckBurger, false);
+}
+
+//Scroll fade elements
+for(var i=0; i < scrollNotVisible.length; i++) {
+    observer.observe(scrollNotVisible[i]);
+}
